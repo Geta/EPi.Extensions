bf5510f9ddc78db35cde95e653da2d5bb1a7973f
Frederik Vig <frederik@geta.no>
2013-11-22 10:31:07 +0100

    Added StripHtml extension method

---
+++ Geta.EPi.Cms/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using EPiServer.Core.Html;
+
+namespace Geta.EPi.Cms.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string StripHtml(string htmlText, int maxLength = int.MaxValue)
+        {
+            if (string.IsNullOrWhiteSpace(htmlText))
+            {
+                return htmlText;
+            }
+
+            return TextIndexer.StripHtml(htmlText, maxLength);
+        }
+    }
+}
