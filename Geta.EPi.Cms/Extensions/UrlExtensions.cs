3c39d76ae290a637dffd3095baac1e8e683a7249
Māris Krivtežs <marisks@gmail.com>
2013-12-03 17:42:20 +0200

    Added extension to get external Uri from URL

---
+++ Geta.EPi.Cms/Extensions/UrlExtensions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web;
+using EPiServer;
+
+namespace Geta.EPi.Cms.Extensions
+{
+    public static class UrlExtensions
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url">Url</param>
+        /// <returns></returns>
+        public static Uri ToExternalUri(this Url url)
+        {
+            if (url == null)
+            {
+                throw new ArgumentNullException("url");
+            }
+
+            if (url.IsEmpty())
+            {
+                throw new ArgumentException("Provided URL is empty");
+            }
+
+            if (url.IsAbsoluteUri)
+            {
+                return url.Uri;
+            }
+
+            var baseUri = GetBaseUri();
+            return new Uri(baseUri, url.Uri);
+        }
+
+        private static Uri GetBaseUri()
+        {
+            if (HttpContext.Current == null)
+            {
+                return EPiServer.Configuration.Settings.Instance.SiteUrl;
+            }
+
+            return new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority));
+        }
+    }
+}
