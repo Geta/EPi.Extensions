d400b0455a90b25384c23e7da658053e0d600ce6
Frederik Vig <frederik@geta.no>
2013-12-10 14:13:28 +0100

    Updated to latest version of EPiServer CMS Moved ContentReference extensions into separate class

--- Geta.EPi.Cms/Extensions/ContentExtensions.cs
+++ Geta.EPi.Cms/Extensions/ContentExtensions.cs
@@ -1,13 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using EPiServer;
-using EPiServer.Configuration;
 using EPiServer.Core;
 using EPiServer.Filters;
 using EPiServer.Framework.Web;
 using EPiServer.ServiceLocation;
-using EPiServer.Web.Routing;
 
 namespace Geta.EPi.Cms.Extensions
 {
@@ -40,76 +37,6 @@ namespace Geta.EPi.Cms.Extensions
             }
 
 			return Enumerable.Empty<T>();
-		}
-
-		public static IEnumerable<PageData> GetChildren(this ContentReference contentLink)
-		{
-			return contentLink.GetChildren<PageData>();
-		}
-
-		public static IEnumerable<T> GetChildren<T>(this ContentReference contentLink) where T : IContentData
-		{
-			if (!contentLink.IsNullOrEmptyContentReference())
-			{
-				var repository = ServiceLocator.Current.GetInstance<IContentLoader>();
-				return repository.GetChildren<T>(contentLink);
-			}
-
-			return Enumerable.Empty<T>();
-		}
-
-		public static PageData GetPage(this ContentReference contentLink)
-		{
-			return contentLink.GetPage<PageData>();
-		}
-
-		public static T GetPage<T>(this ContentReference contentLink) where T: PageData
-		{
-			if (!contentLink.IsNullOrEmptyContentReference())
-			{
-				var loader = ServiceLocator.Current.GetInstance<IContentLoader>();
-				return loader.Get<PageData>(contentLink) as T;
-			}
-
-			return null;
-		}
-
-		public static string GetFriendlyUrl(this ContentReference contentLink, bool includeHost = false)
-		{
-			if (!contentLink.IsNullOrEmptyContentReference())
-			{
-				var urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
-				var url = urlResolver.GetVirtualPath(contentLink);
-
-				if (!includeHost)
-				{
-					return url;
-				}
-
-				var urlBuilder = new UrlBuilder(url);
-
-				if (HttpContext.Current != null)
-				{
-					urlBuilder.Scheme = HttpContext.Current.Request.Url.Scheme;
-					urlBuilder.Host = HttpContext.Current.Request.Url.Host;
-					urlBuilder.Port = HttpContext.Current.Request.Url.Port;
-				}
-				else
-				{
-					urlBuilder.Scheme = Settings.Instance.SiteUrl.Scheme;
-					urlBuilder.Host = Settings.Instance.SiteUrl.Host;
-					urlBuilder.Port = Settings.Instance.SiteUrl.Port;
-				}
-
-				return urlBuilder.ToString();
-			}
-
-			return string.Empty;
-		}
-
-		public static bool IsNullOrEmptyContentReference(this ContentReference contentLink)
-		{
-			return ContentReference.IsNullOrEmpty(contentLink);
 		}
 
 		private static bool VisibleInMenu(IContent content)
