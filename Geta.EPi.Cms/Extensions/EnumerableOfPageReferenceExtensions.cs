0ec746f2469c3b358e0b30c68a21a6b2fbf92d79
Māris Krivtežs <marisks@gmail.com>
2014-02-05 15:50:09 +0200

    Moved page reference enumeration extensions from coop

--- Geta.EPi.Cms/Extensions/EnumerableOfPageReferenceExtensions.cs
+++ Geta.EPi.Cms/Extensions/EnumerableOfPageReferenceExtensions.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections.Generic;
 using EPiServer.Core;
 
-namespace Geta.EPi.Cms.Coop
+namespace Geta.EPi.Cms.Extensions
 {
-    public static class EnumerableExtensions
+    public static class EnumerableOfPageReferenceExtensions
     {
-        
-
         public static PageDataCollection ToPageDataCollection(this IEnumerable<PageReference> pageReferences)
         {
             return new PageDataCollection(pageReferences);
