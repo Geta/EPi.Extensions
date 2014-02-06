1b9ab2371b010306f6f20a3803735da8496be590
Māris Krivtežs <marisks@gmail.com>
2014-02-06 14:16:19 +0200

    Documenting content extensions

--- Geta.EPi.Extensions/ContentExtensions.cs
+++ Geta.EPi.Extensions/ContentExtensions.cs
@@ -6,43 +6,49 @@ using EPiServer.Filters;
 using EPiServer.Framework.Web;
 using EPiServer.ServiceLocation;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
-	public static class ContentExtensions
-	{
-		/// <summary>
-		/// Filters content which should not be visible to the user.
-		/// </summary>
-		public static IEnumerable<T> FilterForDisplay<T>(this IEnumerable<T> content, bool requirePageTemplate = false, bool requireVisibleInMenu = false) where T : IContent
-		{
-            if (content != null)
+    public static class ContentExtensions
+    {
+        /// <summary>
+        ///     Filters content which should not be visible to the user.
+        /// </summary>
+        /// <typeparam name="T">Type of content (IContent).</typeparam>
+        /// <param name="content">Enumeration of content instances to filter.</param>
+        /// <param name="requirePageTemplate">Mark if should include only content with template.</param>
+        /// <param name="requireVisibleInMenu">Mark if should include only pages with mark VisibleInMenu = true.</param>
+        /// <returns>Returns enumeration of filtered content.</returns>
+        public static IEnumerable<T> FilterForDisplay<T>(this IEnumerable<T> content,
+            bool requirePageTemplate = false,
+            bool requireVisibleInMenu = false)
+            where T : IContent
+        {
+            if (content == null) return Enumerable.Empty<T>();
+
+            var contentRepository = ServiceLocator.Current.GetInstance<IContentRepository>();
+            var publishedFilter = new FilterPublished(contentRepository);
+            var accessFilter = new FilterAccess();
+            content = content.Where(x => !publishedFilter.ShouldFilter(x) && !accessFilter.ShouldFilter(x));
+
+            if (requirePageTemplate)
             {
-				var accessFilter = new FilterAccess();
-				var publishedFilter = new FilterPublished(ServiceLocator.Current.GetInstance<IContentRepository>());
-				content = content.Where(x => !publishedFilter.ShouldFilter(x) && !accessFilter.ShouldFilter(x));
-
-				if (requirePageTemplate)
-				{
-					var templateFilter = ServiceLocator.Current.GetInstance<FilterTemplate>();
-					templateFilter.TemplateTypeCategories = TemplateTypeCategories.Page;
-					content = content.Where(x => !templateFilter.ShouldFilter(x));
-				}
-
-				if (requireVisibleInMenu)
-				{
-					content = content.Where(x => VisibleInMenu(x));
-				}
-
-				return content;
+                var templateFilter = ServiceLocator.Current.GetInstance<FilterTemplate>();
+                templateFilter.TemplateTypeCategories = TemplateTypeCategories.Page;
+                content = content.Where(x => !templateFilter.ShouldFilter(x));
             }
 
-			return Enumerable.Empty<T>();
-		}
+            if (requireVisibleInMenu)
+            {
+                content = content.Where(x => VisibleInMenu(x));
+            }
 
-		private static bool VisibleInMenu(IContent content)
-		{
-			var page = content as PageData;
-			return page == null || page.VisibleInMenu;
-		}
-	}
+            return content;
+        }
+
+        private static bool VisibleInMenu(IContent content)
+        {
+            var page = content as PageData;
+            return page == null || page.VisibleInMenu;
+        }
+    }
 }
