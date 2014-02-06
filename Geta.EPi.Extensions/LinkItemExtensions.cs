53969c5ce373e1d5743c0e1f3551067e159322b4
Māris Krivtežs <marisks@gmail.com>
2014-02-06 16:06:40 +0200

    Documenting link item extensions

--- Geta.EPi.Extensions/LinkItemExtensions.cs
+++ Geta.EPi.Extensions/LinkItemExtensions.cs
@@ -3,13 +3,18 @@ using EPiServer.Core;
 using EPiServer.SpecializedProperties;
 using EPiServer.Web;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
     public static class LinkItemExtensions
     {
-        public static ContentReference GetContentReference(this LinkItem target)
+        /// <summary>
+        /// Returns ContentReference for provided LinkItem if it is EPiServer page otherwise returns EmptyReference.
+        /// </summary>
+        /// <param name="source">Source LinkItem for which to return content reference.</param>
+        /// <returns>Returns ContentReference for provided LinkItem if it is EPiServer page otherwise returns EmptyReference.</returns>
+        public static ContentReference GetContentReference(this LinkItem source)
         {
-            var urlBuilder = new UrlBuilder(target.Href);
+            var urlBuilder = new UrlBuilder(source.Href);
             return PermanentLinkMapStore.ToMapped(urlBuilder) 
                 ? PermanentLinkUtility.GetContentReference(urlBuilder) 
                 : ContentReference.EmptyReference;
