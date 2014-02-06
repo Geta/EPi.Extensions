43f1d6e14279d7b7b88cc18cee7300165a4165e4
Māris Krivtežs <marisks@gmail.com>
2014-02-06 15:20:14 +0200

    Documenting enumerable extensions

--- Geta.EPi.Extensions/EnumerableExtensions.cs
+++ Geta.EPi.Extensions/EnumerableExtensions.cs
@@ -6,11 +6,30 @@ namespace Geta.EPi.Extensions
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<TResult> SafeOfType<TResult>(this IEnumerable source)
+        /// <summary>
+        /// Filters the elements of an <see cref="T:System.Collections.IEnumerable"/> based on a specified type.
+        /// Returns empty sequence if source sequence is null.
+        /// NOTE: Helper extension to work with legacy APIs which might return null references of IEnumerable.
+        /// </summary>
+        /// <typeparam name="T">The type to filter the elements of the sequence on.</typeparam>
+        /// <param name="source">The <see cref="T:System.Collections.IEnumerable"/> whose elements to filter.</param>
+        /// <returns>An <see cref="T:System.Collections.Generic.IEnumerable`1"/> that contains elements from the input sequence of type <typeparamref name="T"/>.</returns>
+        public static IEnumerable<T> SafeOfType<T>(this IEnumerable source)
         {
-	        return source == null ? Enumerable.Empty<TResult>() : source.OfType<TResult>();
+	        return source == null ? Enumerable.Empty<T>() : source.OfType<T>().OrEmptyIfNull();
+            
         }
 
+        /// <summary>
+        /// Returns empty sequence if source sequence is null otherwise returns source sequence.
+        /// NOTE: Helper extension to work with legacy APIs which might return null references of IEnumerable.
+        /// </summary>
+        /// <typeparam name="T">The type of elements of the sequence.</typeparam>
+        /// <param name="source">The <see cref="T:System.Collections.Generic.IEnumerable`1"/> to check.</param>
+        /// <returns>
+        /// An <see cref="T:System.Collections.Generic.IEnumerable`1"/> that contains elements from the input sequence of type <typeparamref name="T"/>
+        /// or empty sequence.
+        /// </returns>
         public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> source)
         {
             return source ?? Enumerable.Empty<T>();
