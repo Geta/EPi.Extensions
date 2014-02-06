4a8d02088db16d55939aa2780a9ae588891c90a4
Māris Krivtežs <marisks@gmail.com>
2014-02-06 15:04:18 +0200

    Documenting collection extensions

---
+++ Geta.EPi.Extensions/CollectionExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace Geta.EPi.Extensions
+{
+    public static class CollectionExtensions
+    {
+        /// <summary>
+        /// Appends new item to the collection and returns instance of collection.
+        /// </summary>
+        /// <typeparam name="TCollection">Type of collection to modify.</typeparam>
+        /// <typeparam name="TItem">Type of item to append.</typeparam>
+        /// <param name="collection">Collection to modify.</param>
+        /// <param name="item">Item to append.</param>
+        /// <returns>Modified collection.</returns>
+        public static TCollection Append<TCollection, TItem>(this TCollection collection, TItem item) 
+            where TCollection : ICollection<TItem>
+        {
+            collection.Add(item);
+            return collection;
+        }
+    }
+}
--- Geta.EPi.Extensions/EnumerableExtensions.cs
+++ Geta.EPi.Extensions/EnumerableExtensions.cs
@@ -2,19 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
     public static class EnumerableExtensions
     {
         public static IEnumerable<TResult> SafeOfType<TResult>(this IEnumerable source)
         {
 	        return source == null ? Enumerable.Empty<TResult>() : source.OfType<TResult>();
-        }
-
-	    public static TCollection Append<TCollection, TItem>(this TCollection collection, TItem item) where TCollection : ICollection<TItem>
-        {
-            collection.Add(item);
-            return collection;
         }
 
         public static IEnumerable<T> OrEmptyIfNull<T>(this IEnumerable<T> source)
