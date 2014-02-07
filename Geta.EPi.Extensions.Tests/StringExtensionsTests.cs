2e27f34ff621331e0b13c2c869c4a3e312063f0d
Māris Krivtežs <marisks@gmail.com>
2014-02-07 11:54:34 +0200

    Tests for slug generation

--- Geta.EPi.Extensions.Tests/StringExtensionsTests.cs
+++ Geta.EPi.Extensions.Tests/StringExtensionsTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using Xunit;
+using Xunit.Extensions;
 
 namespace Geta.EPi.Extensions.Tests
 {
@@ -7,14 +8,48 @@ namespace Geta.EPi.Extensions.Tests
     {
         public class GenerateSlugTests
         {
-            [Fact]
-            public void it_should_return_slug_with_10_chars_if_max_length_is_10()
-            {
-                var expected = 10;
 
-                var result = "Some long string here to use".GenerateSlug(expected);
+            [InlineData(0)]
+            [InlineData(10)]
+            [InlineData(100)]
+            [Theory]
+            public void it_should_return_slug_with_less_or_equal_number_chars_than_max_length(int expected)
+            {
+                var result = "Some long string here to use. And some more text.".GenerateSlug(expected);
 
                 result.Length.Should().BeLessOrEqualTo(expected);
+            }
+
+            [Fact]
+            public void it_should_separate_words_in_camel_case_with_dash()
+            {
+                var result = "SomeCamelCaseString".GenerateSlug();
+
+                result.Should().Be("some-camel-case-string");
+            }
+
+            [Fact]
+            public void it_should_remove_non_alphanumeric_chars()
+            {
+                var result = "String with %&*( some 23 &%$ symbols.".GenerateSlug();
+
+                result.Should().Be("string-with-some-23-symbols");
+            }
+
+            [Fact]
+            public void it_should_replace_whitespace_with_dash()
+            {
+                var result = "I have spaces and\ttabs here".GenerateSlug();
+
+                result.Should().Be("i-have-spaces-and-tabs-here");
+            }
+
+            [Fact]
+            public void it_should_lowercase_letters()
+            {
+                var result = "I have UPPER and Camel".GenerateSlug();
+
+                result.Should().Be("i-have-upper-and-camel");
             }
         }
     }
