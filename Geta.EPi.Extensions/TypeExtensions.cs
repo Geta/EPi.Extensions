9401bd2e551a9bd7b324fa63dd0bf212465a4afc
Māris Krivtežs <marisks@gmail.com>
2014-02-07 10:18:33 +0200

    Documenting Type extensions

--- Geta.EPi.Extensions/TypeExtensions.cs
+++ Geta.EPi.Extensions/TypeExtensions.cs
@@ -2,10 +2,18 @@
 using EPiServer.DataAbstraction;
 using EPiServer.ServiceLocation;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
+    /// <summary>
+    /// Type extensions
+    /// </summary>
     public static class TypeExtensions
     {
+        /// <summary>
+        /// Returns PageType for provided Type.
+        /// </summary>
+        /// <param name="pageType">Type for which to lookup PageType.</param>
+        /// <returns>PageType instance if found.</returns>
         public static PageType GetPageType(this Type pageType)
         {
             return ServiceLocator.Current.GetInstance<PageTypeRepository>().Load(pageType);
