9b05bcc0682095d005450165468f1381bfe06c5d
Māris Krivtežs <marisks@gmail.com>
2014-02-07 09:42:46 +0200

    documented page reference extensions

--- Geta.EPi.Extensions/PageReferenceExtensions.cs
+++ Geta.EPi.Extensions/PageReferenceExtensions.cs
@@ -1,17 +1,29 @@
 ﻿using EPiServer.Core;
-using Geta.EPi.Extensions;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
+    /// <summary>
+    /// PageReference extensions.
+    /// </summary>
     public static class PageReferenceExtensions
     {
+        /// <summary>
+        /// Returns string representation of page LinkURL property for provided PageReference.
+        /// </summary>
+        /// <param name="pageReference"></param>
+        /// <returns></returns>
 		public static string GetLinkUrl(this PageReference pageReference)
 		{
 			var page = pageReference.GetPage();
 			return page != null ? page.LinkURL : string.Empty;
 		}
 
-	    public static bool IsNullOrEmptyPageReference(this PageReference pageReference)
+        /// <summary>
+        /// Indicates whether the specified page reference is null or an EmptyReference.
+        /// </summary>
+        /// <param name="pageReference">Page reference to test.</param>
+        /// <returns>true if page reference is null or EmptyReference else false</returns>
+	    public static bool IsNullOrEmpty(this PageReference pageReference)
         {
             return PageReference.IsNullOrEmpty(pageReference);
         }
