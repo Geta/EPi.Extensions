e3fb6a10a14cbdf1011637c53b6c292d92122790
Māris Krivtežs <marisks@gmail.com>
2014-02-07 10:53:42 +0200

    Documented Url extensions

--- Geta.EPi.Extensions/UrlExtensions.cs
+++ Geta.EPi.Extensions/UrlExtensions.cs
@@ -6,15 +6,19 @@ using EPiServer.Core;
 using EPiServer.Web;
 using EPiServer.Web.Routing;
 
-namespace Geta.EPi.Cms.Extensions
+namespace Geta.EPi.Extensions
 {
+    /// <summary>
+    ///     Url extensions.
+    /// </summary>
     public static class UrlExtensions
     {
         /// <summary>
-        /// Creates external Uri from provided Url. Uses HttpContext if available, othervise uses EPiServer settings' SiteUrl
+        ///     Creates external Uri from provided Url.
+        ///     Uses HttpContext if available, othervise uses EPiServer SiteDefinition SiteUrl.
         /// </summary>
-        /// <param name="url">Url</param>
-        /// <returns>External Uri for Url</returns>
+        /// <param name="url">Url for which to create Uri.</param>
+        /// <returns>External Uri for Url.</returns>
         public static Uri ToExternalUri(this Url url)
         {
             if (url == null)
@@ -36,21 +40,36 @@ namespace Geta.EPi.Cms.Extensions
             return new Uri(baseUri, url.Uri);
         }
 
+        /// <summary>
+        ///     Creates Html string of provided Url.
+        /// </summary>
+        /// <param name="url">Url for which to create Html string.</param>
+        /// <returns>Html string with Url if Url is not null. Otherwise returns empty string.</returns>
         public static IHtmlString ToIHtmlString(this Url url)
         {
-            if (url == null)
-            {
-                return MvcHtmlString.Empty;
-            }
-
-            return MvcHtmlString.Create(url.ToString());
+            return url == null
+                ? MvcHtmlString.Empty
+                : MvcHtmlString.Create(url.ToString());
         }
 
+        /// <summary>
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
         public static string Resolve(this Url url)
         {
             return url.Resolve(null);
         }
 
+        /// <summary>
+        ///     Returns string representation of URL for provided Url instance and language.
+        /// </summary>
+        /// <param name="url">Url for which to create string URL.</param>
+        /// <param name="language">Language code for which to create URL.</param>
+        /// <returns>
+        ///     String representation of URL. If Url represents EPiServer page it creates Url with provided language otherwise
+        ///     returns string representation of <paramref name="url" />
+        /// </returns>
         public static string Resolve(this Url url, string language)
         {
             if (url == null)
@@ -72,7 +91,7 @@ namespace Geta.EPi.Cms.Extensions
         {
             if (HttpContext.Current == null)
             {
-                return EPiServer.Configuration.Settings.Instance.SiteUrl;
+                return SiteDefinition.Current.SiteUrl;
             }
 
             return new Uri(HttpContext.Current.Request.Url.GetLeftPart(UriPartial.Authority));
