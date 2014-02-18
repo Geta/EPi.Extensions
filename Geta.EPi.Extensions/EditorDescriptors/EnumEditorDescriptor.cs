2a11537f11b8422c901e2dcabf1689304d60e3e4
Frederik Vig <frederik@geta.no>
2014-02-18 15:36:23 +0100

    Added enum editor descriptor. Based on: http://joelabrahamsson.com/enum-properties-with-episerver/

---
+++ Geta.EPi.Extensions/EditorDescriptors/EnumEditorDescriptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using EPiServer.Shell.ObjectEditing;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+
+namespace Geta.EPi.Extensions.EditorDescriptors
+{
+    /// <summary>
+    /// EPiServer Editor descriptor for creating custom properties with enum types as options
+    /// Source: http://joelabrahamsson.com/enum-properties-with-episerver/
+    /// </summary>
+    /// <typeparam name="TEnum">Enum type with options</typeparam>
+    public class EnumEditorDescriptor<TEnum> : EditorDescriptor
+    {
+        public override void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            SelectionFactoryType = typeof(EnumSelectionFactory<TEnum>);
+
+            ClientEditingClass = "epi.cms.contentediting.editors.SelectionEditor";
+
+            base.ModifyMetadata(metadata, attributes);
+        }
+    }
+}
---
+++ Geta.EPi.Extensions/EditorDescriptors/EnumSelectionFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using EPiServer.Framework.Localization;
+using EPiServer.Shell.ObjectEditing;
+
+namespace Geta.EPi.Extensions.EditorDescriptors
+{
+    /// <summary>
+    /// Selection factory for enum types used by EPiServer Properties
+    /// </summary>
+    /// <typeparam name="TEnum">Enum type with options</typeparam>
+    public class EnumSelectionFactory<TEnum> : ISelectionFactory
+    {
+        public IEnumerable<ISelectItem> GetSelections(
+            ExtendedMetadata metadata)
+        {
+            var values = Enum.GetValues(typeof(TEnum));
+            foreach (var value in values)
+            {
+                yield return new SelectItem
+                {
+                    Text = GetValueName(value),
+                    Value = value
+                };
+            }
+        }
+
+        private string GetValueName(object value)
+        {
+            var staticName = Enum.GetName(typeof(TEnum), value);
+
+            string localizationPath = string.Format("/property/enum/{0}/{1}", typeof(TEnum).Name.ToLowerInvariant(), staticName.ToLowerInvariant());
+
+            string localizedName;
+            if (LocalizationService.Current.TryGetString(localizationPath, out localizedName))
+            {
+                return localizedName;
+            }
+
+            return staticName;
+        }
+    }
+}
