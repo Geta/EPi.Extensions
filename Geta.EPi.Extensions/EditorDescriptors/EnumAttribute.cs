165cc399e5ce5f64baf2ccd4e1c1731b1a292955
Filip Hammerstad <filip@geta.no>
2014-05-23 13:57:23 +0200

    Added new and improved way to add Enum attribute. Library now dependent on Episerver.CMS.UI as well.

---
+++ Geta.EPi.Extensions/EditorDescriptors/EnumAttribute.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.Mvc;
+using EPiServer.Shell.ObjectEditing;
+
+namespace Geta.EPi.Extensions.EditorDescriptors
+{
+    /// <summary>
+    /// EPiServer Editor descriptor for creating custom properties with enum types as options
+    /// Source: http://world.episerver.com/Blogs/Linus-Ekstrom/Dates/2014/5/Enum-properties-for-EPiServer-75/
+    /// </summary>    
+    public class EnumAttribute : SelectOneAttribute, IMetadataAware
+    {
+        /// <summary>
+        /// Enum type in EnumAttribute
+        /// </summary>
+        public Type EnumType { get; set; }
+
+        /// <summary>
+        /// Enum Attribute
+        /// </summary>
+        /// <param name="enumType">Type of enum</param>
+        public EnumAttribute(Type enumType)
+        {
+            EnumType = enumType;
+        }
+
+        public new void OnMetadataCreated(ModelMetadata metadata)
+        {
+            SelectionFactoryType = typeof(EnumSelectionFactory<>).MakeGenericType(EnumType);
+            base.OnMetadataCreated(metadata);
+        }
+    }
+
+}
--- Geta.EPi.Extensions/EditorDescriptors/EnumEditorDescriptor.cs
+++ Geta.EPi.Extensions/EditorDescriptors/EnumEditorDescriptor.cs
@@ -6,10 +6,12 @@ using EPiServer.Shell.ObjectEditing.EditorDescriptors;
 namespace Geta.EPi.Extensions.EditorDescriptors
 {
     /// <summary>
+    /// Obsolete.
     /// EPiServer Editor descriptor for creating custom properties with enum types as options
     /// Source: http://joelabrahamsson.com/enum-properties-with-episerver/
     /// </summary>
     /// <typeparam name="TEnum">Enum type with options</typeparam>
+    [Obsolete("Replaced by EnumAttribute in same namespace")]
     public class EnumEditorDescriptor<TEnum> : EditorDescriptor
     {
         public override void ModifyMetadata(ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
