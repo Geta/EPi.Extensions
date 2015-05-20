c6f15116e0c4e6ee4a6484f3193e44bf1927ae38
Frederik Vig <frederik@geta.no>
2015-05-20 12:42:14 -0400

    New extension method: GetFirstChild (IContentLoader and IContentRepository)

---
+++ Geta.EPi.Extensions/ContentLoaderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EPiServer;
+using EPiServer.Core;
+
+namespace Geta.EPi.Extensions
+{
+    public static class ContentLoaderExtensions
+    {
+        /// <summary>
+        /// Gets the first child of of the content item represented by the provided reference. 
+        /// </summary>
+        /// <param name="contentLoader"></param>
+        /// <param name="contentReference"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetFirstChild<T>(this IContentLoader contentLoader, ContentReference contentReference) where T : IContentData
+        {
+            return contentLoader.GetChildren<T>(contentReference).FirstOrDefault();
+        }
+    }
+}
---
+++ Geta.EPi.Extensions/ContentRepositoryExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EPiServer;
+using EPiServer.Core;
+
+namespace Geta.EPi.Extensions
+{
+    public static class ContentRepositoryExtensions
+    {
+        /// <summary>
+        /// Gets the first child of of the content item represented by the provided reference. 
+        /// </summary>
+        /// <param name="contentRepository"></param>
+        /// <param name="contentReference"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetFirstChild<T>(this IContentRepository contentRepository, ContentReference contentReference) where T : IContentData
+        {
+            return contentRepository.GetChildren<T>(contentReference).FirstOrDefault();
+        }
+    }
+}
