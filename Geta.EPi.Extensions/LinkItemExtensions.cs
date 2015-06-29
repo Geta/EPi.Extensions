3fbbc03a0d014c817ff6d6f64337710e4354857f
Dzulqarnain Nasir <dzul1983@gmail.com>
2015-06-29 21:06:37 +0300

    Updated LinkItemExtension. * Renamed method to GetFriendlyUrl. * Added includeHost boolean for URL generation.

--- Geta.EPi.Extensions/LinkItemExtensions.cs
+++ Geta.EPi.Extensions/LinkItemExtensions.cs
@@ -41,14 +41,15 @@ namespace Geta.EPi.Extensions
         ///     Returns friendly URL if item is EPiServer content, otherwise returns the original Href property value.
         /// </summary>
         /// <param name="linkItem">Source LinkItem for which to return external URL.</param>
+        /// <param name="includeHost">Mark if include host name in the url.</param>
         /// <returns>Returns friendly URL if item is EPiServer content, otherwise returns the original Href property value.</returns>
-        public static string GetExternalUrl(this LinkItem linkItem)
+        public static string GetFriendlyUrl(this LinkItem linkItem, bool includeHost = false)
         {
             var contentReference = linkItem.ToContentReference();
 
             return contentReference == ContentReference.EmptyReference
                 ? linkItem.Href
-                : contentReference.GetFriendlyUrl();
+                : contentReference.GetFriendlyUrl(includeHost);
         }
     }
 }
