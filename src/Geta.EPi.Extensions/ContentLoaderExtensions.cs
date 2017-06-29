07a04991014bc0cc226fc0514c9da68932fa00ca
Frederik Vig <frederik@geta.no>
2017-06-29 09:53:34 -0400

    Added overload culture for GetFirstChild()

--- src/Geta.EPi.Extensions/ContentLoaderExtensions.cs
+++ src/Geta.EPi.Extensions/ContentLoaderExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Globalization;
+using System.Linq;
 using EPiServer;
 using EPiServer.Core;
 
@@ -16,6 +17,19 @@ namespace Geta.EPi.Extensions
         public static T GetFirstChild<T>(this IContentLoader contentLoader, ContentReference contentReference) where T : IContentData
         {
             return contentLoader.GetChildren<T>(contentReference).FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Gets the first child of of the content item represented by the provided reference. 
+        /// </summary>
+        /// <param name="contentLoader"></param>
+        /// <param name="contentReference"></param>
+        /// <param name="culture"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static T GetFirstChild<T>(this IContentLoader contentLoader, ContentReference contentReference, CultureInfo culture) where T : IContentData
+        {
+            return contentLoader.GetChildren<T>(contentReference, culture).FirstOrDefault();
         }
     }
 }
