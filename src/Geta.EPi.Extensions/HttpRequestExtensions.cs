596fa26f1649c451ef2adfc59786b146b9d9de25
Māris Krivtežs <marisks@gmail.com>
2017-11-10 17:41:08 +0200

    Added HttpRequest extensions.
    Added extension to check if request is issued by crawler.

---
+++ src/Geta.EPi.Extensions/HttpRequestExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Geta.EPi.Extensions
+{
+    /// <summary>
+    /// HttpRequest extensions.
+    /// </summary>
+    public static class HttpRequestExtensions
+    {
+        /// <summary>
+        /// Checks if current request is a crawler request.
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        public static bool IsCrawler(this HttpRequest request)
+        {
+            if (string.IsNullOrWhiteSpace(request.UserAgent))
+                return false;
+
+            return Regex.IsMatch(
+                request.UserAgent,
+                @"bot|crawler|baiduspider|80legs|ia_archiver|voyager|curl|wget|yahoo! slurp|mediapartners-google",
+                RegexOptions.IgnoreCase);
+        }
+    }
+}
