6ecd838d676904d51e355bbc0fe7fba3b7061d1e
Māris Krivtežs <marisks@gmail.com>
2017-11-10 17:39:54 +0200

    Added RequestBase extensions.
    Added extension to get user IP address.

---
+++ src/Geta.EPi.Extensions/RequestBaseExtensionMethods.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+
+namespace Geta.EPi.Extensions
+{
+    /// <summary>
+    /// HttpRequestBase extensions.
+    /// </summary>
+    public static class RequestBaseExtensionMethods
+    {
+        /// <summary>
+        /// Returns user IP address.
+        /// </summary>
+        /// <param name="requestBase"></param>
+        /// <returns></returns>
+        public static string GetUserIp(this HttpRequestBase requestBase)
+        {
+            if (requestBase == null)
+            {
+                return string.Empty;
+            }
+
+            //First get HTTP_X_FORWARDED_FOR ip if client is behind a proxy
+            var userIp = requestBase.ServerVariables["HTTP_X_FORWARDED_FOR"];
+
+            if (string.IsNullOrEmpty(userIp))
+            {
+                return requestBase.ServerVariables["REMOTE_ADDR"];
+            }
+
+            var ipArray = userIp.Split(',');
+            return ipArray[0];
+        }
+    }
+}
