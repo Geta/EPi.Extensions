c45d5fa5cb41acac960ace1569d02c29bc05652d
Māris Krivtežs <marisks@gmail.com>
2017-11-10 17:42:01 +0200

    Added extension for view context to check if block is in edit mode.

--- src/Geta.EPi.Extensions/ViewContextExtensions.cs
+++ src/Geta.EPi.Extensions/ViewContextExtensions.cs
@@ -1,4 +1,6 @@
 ﻿using System.Web.Mvc;
+using EPiServer.Web;
+using EPiServer.Web.Routing;
 
 namespace Geta.EPi.Extensions
 {
@@ -8,13 +10,23 @@ namespace Geta.EPi.Extensions
     public static class ViewContextExtensions
     {
         /// <summary>
-        ///     Checks if currently in Block preview controller.
+        /// Checks if currently in Block preview controller.
         /// </summary>
         /// <param name="viewContext"></param>
         /// <returns></returns>
         public static bool IsBlockPreview(this ViewContext viewContext)
         {
             return viewContext.HttpContext.IsBlockPreview();
+        }
+
+        /// <summary>
+        /// Checks if block is in edit mode.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public static bool IsBlockInEditMode(this ViewContext context)
+        {
+            return context.RequestContext.GetContextMode() == ContextMode.Edit && context.IsBlockPreview();
         }
     }
 }
