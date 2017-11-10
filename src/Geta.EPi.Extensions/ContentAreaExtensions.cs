cc9b79a9dc8db67c9b9f7ef81ab130e3c9ecb843
Māris Krivtežs <marisks@gmail.com>
2017-11-10 17:38:08 +0200

    Added content area extensions.
    Added extension to check if has content.

---
+++ src/Geta.EPi.Extensions/ContentAreaExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using EPiServer.Core;
+
+namespace Geta.EPi.Extensions
+{
+    /// <summary>
+    /// Content area extensions.
+    /// </summary>
+    public static class ContentAreaExtensions
+    {
+        /// <summary>
+        /// Checks if content area has content.
+        /// </summary>
+        /// <param name="contentArea">The content area.</param>
+        /// <returns>Returns true if content area has content and false when not.</returns>
+        public static bool HasContent(this ContentArea contentArea)
+        {
+            return contentArea?.FilteredItems != null && contentArea.FilteredItems.Any();
+        }
+    }
+}
