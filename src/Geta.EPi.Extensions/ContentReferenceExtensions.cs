c7eaefe0563bf52ae40a6a369c79280b29afcdee
Māris Krivtežs <marisks@gmail.com>
2018-01-05 14:02:47 +0200

    Refactored url resolving.

--- src/Geta.EPi.Extensions/ContentReferenceExtensions.cs
+++ src/Geta.EPi.Extensions/ContentReferenceExtensions.cs
@@ -131,11 +131,9 @@ namespace Geta.EPi.Extensions
                 urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
             }
 
-            var url = urlResolver.GetUrl(contentReference, language);
-            if (ignoreContextMode)
-            {
-                url = urlResolver.GetUrl(contentReference, language, new VirtualPathArguments { ContextMode = ContextMode.Default });
-            }
+            var url = ignoreContextMode
+                ? urlResolver.GetUrl(contentReference, language, new VirtualPathArguments { ContextMode = ContextMode.Default })
+                : urlResolver.GetUrl(contentReference, language);
 
             return !includeHost ? url : url.GetExternalUrl();
         }
