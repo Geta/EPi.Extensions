65ec3e67e92d64964f60f971e95cfde84346ae3b
Gatis Bergšpics <gatisb@gmail.com>
2019-12-17 14:14:42 +0200

    episerver has this feature remainingUrl, it is ignored by this package #52

--- src/Geta.EPi.Extensions/LinkItemExtensions.cs
+++ src/Geta.EPi.Extensions/LinkItemExtensions.cs
@@ -1,6 +1,6 @@
-﻿using EPiServer;
+﻿using System.Web.WebPages;
+using EPiServer;
 using EPiServer.Core;
-using EPiServer.ServiceLocation;
 using EPiServer.SpecializedProperties;
 using EPiServer.Web.Routing;
 
@@ -15,26 +15,26 @@ namespace Geta.EPi.Extensions
         ///     Returns ContentReference for provided LinkItem if it is EPiServer content otherwise returns EmptyReference.
         /// </summary>
         /// <param name="source">Source LinkItem for which to return content reference.</param>
-        /// <returns>Returns ContentReference for provided LinkItem if it is EPiServer content otherwise returns EmptyReference.</returns>
+        /// <returns>Returns ContentReference for provided LinkItem if it is EPiServer content otherwise returns EmptyReference. Note that remainingUrl part will be lost use with caution.</returns>
         public static ContentReference ToContentReference(this LinkItem source)
         {
             var content = source.ToContent();
 
             return content != null
-                       ? content.ContentLink
-                       : ContentReference.EmptyReference;
+                ? content.ContentLink
+                : ContentReference.EmptyReference;
         }
 
         /// <summary>
         ///     Returns IContent for provided LinkItem if it is EPiServer content otherwise returns null.
         /// </summary>
         /// <param name="source">Source LinkItem for which to return content.</param>
-        /// <returns>Returns IContent for provided LinkItem if it is EPiServer content otherwise returns null.</returns>
+        /// <returns>Returns IContent for provided LinkItem if it is EPiServer content otherwise returns null. Note that remainingUrl part will be lost use with caution.</returns>
         public static IContent ToContent(this LinkItem source)
         {
-            var urlBuilder = new UrlBuilder(source.Href);
-            var resolver = ServiceLocator.Current.GetInstance<UrlResolver>();
-            return resolver.Route(urlBuilder);
+            var urlBuilder = new UrlBuilder(source.GetMappedHref());
+
+            return UrlResolver.Current.Route(urlBuilder);
         }
 
         /// <summary>
@@ -45,11 +45,22 @@ namespace Geta.EPi.Extensions
         /// <returns>Returns friendly URL if item is EPiServer content, otherwise returns the original Href property value.</returns>
         public static string GetFriendlyUrl(this LinkItem linkItem, bool includeHost = false)
         {
-            var contentReference = linkItem.ToContentReference();
+            if (linkItem.Href == null)
+            {
+                return string.Empty;
+            }
 
-            return contentReference == ContentReference.EmptyReference
-                ? linkItem.Href
-                : contentReference.GetFriendlyUrl(includeHost);
+            var url = new Url(linkItem.GetMappedHref());
+            if (url.IsEmpty())
+            {
+                return string.Empty;
+            }
+
+            var friendlyUrl = url.IsAbsoluteUri
+                ? url.ToString()
+                : UrlResolver.Current.GetUrl(url.ToString()) ?? url.ToString();
+
+            return includeHost && !friendlyUrl.IsEmpty() ? friendlyUrl.AddHost() : friendlyUrl;
         }
     }
 }
